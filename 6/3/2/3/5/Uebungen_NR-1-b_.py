f9e29bbae2db5551b29d56f0602b485926e1939e
Maggesss <mbj.kilian@outlook.de>
2022-04-08 16:38:17 +0200

    6.3.2.3.5

---
+++ 6/3/2/3/5/Uebungen_NR-1-b_.py
@@ -0,0 +1,23 @@
+# === Numeric-Check (float) ===
+def isNumeric(probe):
+    try:
+        probe = float(probe)
+        return True
+    except:
+        return False
+
+# === Berechnung des BMI ===
+def bmi_rechnung(weight, height):
+    # abbrechen, wenn keine validen Zeichen / Zahlen eingegeben wurden
+    if isNumeric(weight) or isNumeric(height) == False:
+        return
+    # Berechnung bei Normalfall
+    bmi = float(weight) / (float(height) * float(height))
+    # Output
+    return print(f"Bei einem Gewicht von {weight} und einer Größe von {height} haben Sie einen BMI von {round(bmi, 1)}.")
+
+# loop erstellen und Funktion "aktivieren", in Funktion nach input "fragen"
+loop = "y"
+while (loop == "y"):
+    bmi_rechnung(input("Bitte Gewicht eingeben: "), input("Bitte Größe eingeben: "))
+    loop = input("Nochmal? y/n: ")
